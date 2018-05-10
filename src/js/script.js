2ece0e9fb95954e7e3db8422f064b7580fdbe596
saorim <saori.miura.sm@gmail.com>
2018-05-10 19:03:17 +0900

    ブラウザをドラッグ可能に変更 #5

--- src/js/script.js
+++ src/js/script.js
@@ -48,11 +48,94 @@ $( function() {
 
   }
 
+  function dragControl() {
+
+    var elements = $('.js-dragHeader');
+
+    var x;
+    var y;
+
+    //マウスが要素内で押されたとき、又はタッチされたとき発火
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener('mousedown', mdown, false);
+      elements[i].addEventListener('touchstart', mdown, false);
+    }
+
+    //マウスが押された際の関数
+    function mdown(e) {
+
+      var $dragArea = $(this).parent('.js-draggable');
+
+      $dragArea.addClass('is-dragged');
+
+      //タッチデイベントとマウスのイベントの差異を吸収
+      if(e.type === 'mousedown') {
+        var event = e;
+      } else {
+        var event = e.changedTouches[0];
+      }
+
+      //要素内の相対座標を取得
+      x = event.pageX - $dragArea.offset().left;
+      y = event.pageY - $dragArea.offset().top;
+
+      //ムーブイベントにコールバック
+      document.body.addEventListener('mousemove', mmove, false);
+      document.body.addEventListener('touchmove', mmove, false);
+    }
+
+    //マウスカーソルが動いたときに発火
+    function mmove(e) {
+
+      var drag = document.getElementsByClassName('is-dragged')[0];
+
+      //同様にマウスとタッチの差異を吸収
+      if(e.type === 'mousemove') {
+        var event = e;
+      } else {
+        var event = e.changedTouches[0];
+      }
+
+      //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+      e.preventDefault();
+
+      //マウスが動いた場所に要素を動かす
+      drag.style.top = event.pageY - y + 'px';
+      drag.style.left = event.pageX - x + 'px';
+
+      //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+      drag.addEventListener('mouseup', mup, false);
+      document.body.addEventListener('mouseleave', mup, false);
+      drag.addEventListener('touchend', mup, false);
+      document.body.addEventListener('touchleave', mup, false);
+
+    }
+
+    //マウスボタンが上がったら発火
+    function mup(e) {
+      var drag = document.getElementsByClassName('is-dragged')[0];
+
+      //ムーブベントハンドラの消去
+      if (typeof drag !== 'undefined') {
+        document.body.removeEventListener('mousemove', mmove, false);
+        document.body.removeEventListener('touchmove', mmove, false);
+        drag.removeEventListener('mouseup', mup, false);
+        drag.removeEventListener('touchend', mup, false);
+
+        //クラス名 .is-dragged も消す
+        drag.classList.remove('is-dragged');
+      }
+
+    }
+
+  }
+
 
   // fire when DOM is ready
   $(function() {
     anchorLink();
-    navControl();
+    // navControl();
+    dragControl();
   });
 
   // fire when page is fully loaded
